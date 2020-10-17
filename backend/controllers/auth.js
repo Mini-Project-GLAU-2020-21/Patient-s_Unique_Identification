e6d76fb0b7a8f2396af1bbc82ce582249c18c0a8
Abhinav Bhardwaj <abhinav.bhardwaj_cs18@gla.ac.in>
2020-10-17 19:12:03 +0530

    after creating a signup controller in auth.js controllers

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -5,3 +5,30 @@ var expressJwt = require('express-jwt');
 
 
 
+exports.signup = (req, res) => {
+
+    const error = validationResult(req);
+
+    if(!error.isEmpty()){
+        return res.status(422).json({
+            error: error.array()[0].msg
+        });
+    };
+
+    const patient = new Patient(req.body)
+        patient.save((err, patient) => {
+            if(err || !patient){
+                return res.status(400).json({
+                    err: "NOT able to save user in DB"
+                });
+            }
+            res.json({
+                f_name: patient.f_name,
+                m_name: patient.m_name,
+                l_name: patient.l_name,
+                email: patient.email,
+                upi: patient.upi,
+                id: patient._id
+            });
+        });
+};
