37d4d0665fcf2e193e88c6bdd956ac9bfb390bcb
Abhinav Bhardwaj <abhinav.bhardwaj_cs18@gla.ac.in>
2020-10-17 14:04:44 +0530

    building patient schema

--- backend/models/patient.js
+++ backend/models/patient.js
@@ -4,21 +4,28 @@ const uuidv4 = require("uuid/v4");
 
 var Schema = mongoose.Schema;
 var patientSchema = new Schema({
-    fname: {
+    f_name: {
         type: String,
         required: true,
         maxlength: 15,
         trim: true
     },
-    mname:{
+    m_name:{
         type: String,
         maxlength: 15,
         trim: true
     },
-    lname: {
+    l_name: {
         type: String,
         maxlength:15,
         trim: true
     },
-    
+    email: {
+        type: String,
+        trim:true,
+        unique: true
+    },
+    contact_number: {
+        type: 
+    }
 });
