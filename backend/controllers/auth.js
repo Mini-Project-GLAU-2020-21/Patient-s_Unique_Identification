8c9dbac9e13150ed56e6e1a35834647ec9b5b261
Abhinav Bhardwaj <abhinav.bhardwaj_cs18@gla.ac.in>
2020-10-18 12:56:38 +0530

    debugging

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -5,6 +5,8 @@ var expressJwt = require('express-jwt');
 
 
 
+
+
 exports.signup = (req, res) => {
 
     const error = validationResult(req);
@@ -16,19 +18,65 @@ exports.signup = (req, res) => {
     };
 
     const patient = new Patient(req.body)
-        patient.save((err, patient) => {
+    patient.save((err, patient) => {
             if(err || !patient){
                 return res.status(400).json({
-                    err: "NOT able to save user in DB"
+                    err: "NOT able to save patient in DB"
                 });
             }
             res.json({
-                f_name: patient.f_name,
-                m_name: patient.m_name,
-                l_name: patient.l_name,
+                name: patient.name,
                 email: patient.email,
-                upi: patient.upi,
                 id: patient._id
             });
         });
+};
+
+
+
+
+
+exports.signin = (req, res) => {
+    const error = validationResult(req);
+    const { email, password } = req.body;
+
+    if(!error.isEmpty()){
+        return res.status(422).json({
+            error: error.array()[0].msg
+        });
+    };
+
+    Patient.findOne({email}, (err, patient) =>{
+        if(err || !patient){
+            return res.status(400).json({
+                error: "Patient email does not exists"
+            })
+        }
+
+        if(!patient.authenticate(password)){
+            return res.status(401).json({
+                error: "Email and password do not match"
+            })
+        }
+
+        //Create token
+        const token = jwt.sign({_id: patient._id}, process.env.SECRET_Key);
+        //put token in cookie
+        res.cookie("token", token, {expire: new Date() + 9999});
+
+        //send response on frontend
+        const {_id, name, email, role} = patient;
+        return res.json({ token, patient: {_id, name, email, role}})
+    })
+
+}
+
+
+
+
+exports.signout = (req, res) => {
+    res.clearCookie("token");
+    res.json({
+        message: "User signout successfully"
+    });
 };
