f6eb3c13779710efab304174c7a7c09fe3337646
Abhinav Bhardwaj <abhinav.bhardwaj_cs18@gla.ac.in>
2020-11-01 23:36:56 +0530

    After creating a controller to return patient's own details to frontend.

--- backend/controllers/patient.js
+++ backend/controllers/patient.js
@@ -13,3 +13,12 @@ exports.getPatientById = (req, res, next, id) => {
         next();
     });
 };
+
+
+//to return patient's own details
+exports.getPatientByOwn = (req, res) => {
+    req.profile.salt = undefined;
+    req.profile.encry_password = undefined;
+    req.profile.createdAt = undefined;
+    return res.json(req.profile);
+};
