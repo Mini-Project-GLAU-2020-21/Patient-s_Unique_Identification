1767fffbd047b4f052e6dd776bfd3e8505f32aaf
Abhinav Bhardwaj <abhinav.bhardwaj_cs18@gla.ac.in>
2020-11-01 23:27:18 +0530

    After creating a controller for extracting the data of the patient through id (provided by database).

--- backend/controllers/patient.js
+++ backend/controllers/patient.js
@@ -1,1 +1,15 @@
 const Patient = require("../models/patient");
+
+
+
+exports.getPatientById = (req, res, next, id) => {
+    Patient.findById(id).exec((err, patient) => {
+        if(err || !patient){
+            return res.status(400).json({
+                error: "No patient found in DB!!!"
+            });
+        }
+        req.profile = patient;
+        next();
+    });
+};
