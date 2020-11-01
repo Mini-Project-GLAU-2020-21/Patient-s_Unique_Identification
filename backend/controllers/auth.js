16c9ba8302b84c84d70a65030ebf27fae28ec522
Abhinav Bhardwaj <abhinav.bhardwaj_cs18@gla.ac.in>
2020-11-01 23:06:32 +0530

    After adding isAuthenticated controller to check during some process whether the user is authenticated to do that or not.

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -88,3 +88,15 @@ exports.isSignedIn = expressJwt({
     secret: process.env.SECRET,
     userProperty: "auth"
 });
+
+
+//custom middlewares
+exports.isAuthenticated = (req, res, next) => {
+    let checker = req.profile && req.auth && req.profile._id == req.auth._id;
+    if( !checker ){
+        return res.status(403).json({
+            error: "ACCESS DENIED !!!"
+        });
+    }
+    next();
+}
