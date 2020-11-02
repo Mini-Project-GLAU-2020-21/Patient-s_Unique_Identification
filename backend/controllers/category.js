b6f6977a58b2b4b3f66253f8e62bb950216b7d1f
Abhinav Bhardwaj <abhinav.bhardwaj_cs18@gla.ac.in>
2020-11-03 00:45:00 +0530

    After creating a method to create new category in category.js controllers.

--- backend/controllers/category.js
+++ backend/controllers/category.js
@@ -1,1 +1,14 @@
 const Category = require("../models/category");
+
+
+exports.createCategory = (req, res) => {
+    const category = new Category(req.body);
+    category.save((err, category) => {
+        if(err){
+            return res.status(400).json({
+                error: "Not able to create this category!!!"
+            });
+        }
+        res.json({category});
+    });
+};
