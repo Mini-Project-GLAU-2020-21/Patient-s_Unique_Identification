f848fa8cedfd87dc645d43b97c5da6883533b8b5
Abhinav Bhardwaj <abhinav.bhardwaj_cs18@gla.ac.in>
2020-11-03 00:36:22 +0530

    After creating category's schema in category.js model

--- backend/models/category.js
+++ backend/models/category.js
@@ -1,1 +1,14 @@
 const mongoose = require("mongoose");
+
+const categorySchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            trim: true,
+            maxlength: 20,
+            required: true,
+            unique: true
+        }
+    },
+    {timestamps: true }
+);
