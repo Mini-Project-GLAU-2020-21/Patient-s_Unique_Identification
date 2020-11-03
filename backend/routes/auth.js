af23b0f1bf2a90ed9099b9497c9b008abc92692f
Abhinav Bhardwaj <abhinav.bhardwaj_cs18@gla.ac.in>
2020-11-04 00:08:22 +0530

    Debugging update routes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -12,7 +12,7 @@ router.post(
     [           // if check("name", "name should be atleast of 3 letters.") doesn't works, use   check("name").isLength({ min: 3 }).withMessage('must be at least 3 chars long')
         check("f_name", "First Name should be at least of 3 char.").isLength({ min: 3 }).custom(value => !/\s/.test(value)),
         check("email", "Valid email is required.").isEmail().custom(value => !/\s/.test(value)),
-        check("contact_number", "Valid Mobile Number is required.").isLength({ min: 10 }).isMobilePhone('en-IN'),
+        check("p_contact_number", "Valid Mobile Number is required.").isLength({ min: 10 }).isMobilePhone('en-IN'),
         check("password", "password should be at least 5 char.").custom(value => !/\s/.test(value)).isLength({ min: 5 })
     ], 
     signup
