4157dd1c48e0b813ea3c21e8ba52194d52087b92
saditya2501 <aditya.chuahan_cs18@gla.ac.in>
2020-11-09 23:30:32 +0530

    Initial Commit

--- app/src/main/java/com/aditya/upi/MainActivity.java
+++ app/src/main/java/com/aditya/upi/MainActivity.java
@@ -18,5 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        name = findViewById(R.id.editText);
     }
 }
