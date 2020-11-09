d4b4e8badd3f2f279eab90afefb9c7987d4ad60a
saditya2501 <aditya.chuahan_cs18@gla.ac.in>
2020-11-09 23:34:37 +0530

    Initial Commit

--- app/src/main/java/com/aditya/upi/MainActivity.java
+++ app/src/main/java/com/aditya/upi/MainActivity.java
@@ -2,23 +2,15 @@ package com.aditya.upi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText name,email;
-    Button button;
-    SharedPreferences sp;
-    String namestr,emailstr;
+    Ed
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        name = findViewById(R.id.editText);
     }
 }
