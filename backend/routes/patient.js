151e7d1f208864c877fa18fd0f49efc81e3c3fe4
Abhinav Bhardwaj <abhinav.bhardwaj_cs18@gla.ac.in>
2020-11-11 23:59:38 +0530

    After adding the routes in patient.js routes for uploadDocument controller of patient.js controllers

--- backend/routes/patient.js
+++ backend/routes/patient.js
@@ -3,17 +3,23 @@ const router = express.Router();
 
 
 
-const { getPatientById, getPatientByOwn, getPatientForGuest, updatePatient } = require("../controllers/patient");  
+const { getPatientById, getPatientByOwn, getPatientForGuest, updatePatient, uploadDocument } = require("../controllers/patient");  
 const { isSignedIn, isAdmin, isAuthenticated } = require("../controllers/auth");
 
-
+// params
 router.param("patientId", getPatientById);
 
 
+//actual routes
+
+// get patient routes
 router.get("/patient/:patientId", isSignedIn, isAuthenticated, getPatientByOwn);
 
+// update patient route
 router.put("/patient/:patientId", isSignedIn, isAuthenticated, updatePatient);
 
+// upload document route
+router.put("/patient/uploadDocument/:patientId", isSignedIn, isAuthenticated, uploadDocument);
 
 
 module.exports = router;
