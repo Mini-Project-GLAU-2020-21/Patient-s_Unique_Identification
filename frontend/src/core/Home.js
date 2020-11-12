49ee8c28b7d379342c615abb59dcc24573551e9f
Abhinav Bhardwaj <abhinav.bhardwaj_cs18@gla.ac.in>
2020-11-12 23:31:44 +0530

    After creating a basic home page

---
+++ frontend/src/core/Home.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { API } from '../backend'
+import "../styles.css";
+import Base from './Base';
+
+
+export default function Home() {
+    console.log("API IS", API)
+    return (
+        <Base title="Welcome to Patient's Unique Identification"
+        description="Stay Healthy">
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+
+            <br/>
+            <br/>
+            <br/>
+        
+        </Base>
+    );
+};
