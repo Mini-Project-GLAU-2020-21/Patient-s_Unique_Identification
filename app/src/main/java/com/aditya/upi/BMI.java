aacc7c91db28ba66214bb0c939b9bf92aea57da4
saditya2501 <aditya.chuahan_cs18@gla.ac.in>
2020-11-15 23:38:39 +0530

    Initial Commit

---
+++ app/src/main/java/com/aditya/upi/BMI.java
@@ -0,0 +1,14 @@
+package com.aditya.upi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BMI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_b_m_i);
+    }
+}
