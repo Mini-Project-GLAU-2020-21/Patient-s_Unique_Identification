6b1fad2c7903305303185f4bc59d22be5248bc2d
saditya2501 <aditya.chuahan_cs18@gla.ac.in>
2020-11-18 00:41:33 +0530

    Initial Commit

--- app/src/main/java/com/aditya/upi/Dash.java
+++ app/src/main/java/com/aditya/upi/Dash.java
@@ -2,7 +2,9 @@ package com.aditya.upi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Dash extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class Dash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash);
+    }
+
+    public void btn_BMI(View view) {
+        startActivity(new Intent(getApplicationContext(),Bmi.class));
     }
 }
