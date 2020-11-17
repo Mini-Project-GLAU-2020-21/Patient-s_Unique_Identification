222ba920593e2b0698f9b134d69f999db59c7094
saditya2501 <aditya.chuahan_cs18@gla.ac.in>
2020-11-18 00:02:09 +0530

    Initial Commit

--- app/src/main/java/com/aditya/upi/Bmi.java
+++ app/src/main/java/com/aditya/upi/Bmi.java
@@ -10,5 +10,34 @@ public class Bmi extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bmi);
+
+    }
+
+    public float BMICalculate(float weight,float height)
+    {
+        return weight/(height * height);
+    }
+
+    public String interpretateBMI(float BMIvalues)
+    {
+        if( BMIvalues < 16)
+        {
+            return "Severly UnderWeight";
+        }
+        else if (BMIvalues < 18.5 )
+        {
+            return "UnderWeight";
+        }
+        else if (BMIvalues <25)
+        {
+            return "Normal";
+
+        }
+        else if (BMIvalues < 30)
+        {
+            return "OverWeight";
+        }
+        else
+            return "Obese";
     }
 }
