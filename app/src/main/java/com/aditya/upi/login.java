6e5e519083e8a87c5a195f9e375e5f13c4326415
saditya2501 <aditya.chuahan_cs18@gla.ac.in>
2020-11-20 09:18:42 +0530

    Initial Commit

--- app/src/main/java/com/aditya/upi/login.java
+++ app/src/main/java/com/aditya/upi/login.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class login extends AppCompatActivity {
 
@@ -12,6 +14,35 @@ public class login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login); getSupportActionBar().setTitle("Login  Page");
+
+        final EditText Email,Pass;
+        Button btnSignIn;
+
+        Email=(EditText) findViewById(R.id.email);
+        Pass=(EditText) findViewById(R.id.pass);
+
+        btnSignIn=(Button) findViewById(R.id.btnsign);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (Email.equals(""))
+                {
+                    Email.setError("Enter your Email id");
+                    Email.requestFocus();
+                    return;
+                }
+                if (Pass.equals(""))
+                {
+                    Pass.setError("Enter your Password");
+                    Pass.requestFocus();
+                    return;
+                }
+            }
+        });
+
+
     }
 
     public void btn_signupForm(View view) {
