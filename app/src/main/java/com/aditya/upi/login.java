7246b2288de6746f7358cc4fb59a0ce3f58a1375
saditya2501 <aditya.chuahan_cs18@gla.ac.in>
2020-11-22 12:21:05 +0530

    Initial Commit

--- app/src/main/java/com/aditya/upi/login.java
+++ app/src/main/java/com/aditya/upi/login.java
@@ -2,54 +2,92 @@ package com.aditya.upi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class login extends AppCompatActivity {
+import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.Objects;
+
+public class login extends AppCompatActivity {
+    private Button login;
+
+    private EditText emailid,password;
+    private TextView signupp;
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login); getSupportActionBar().setTitle("Login  Page");
+        setContentView(R.layout.activity_login);
+        getvalue();
 
-        final EditText Email,Pass;
-        Button btnSignIn;
-
-        Email=(EditText) findViewById(R.id.email);
-        Pass=(EditText) findViewById(R.id.pass);
-
-        btnSignIn=(Button) findViewById(R.id.btnsign);
-
-        btnSignIn.setOnClickListener(new View.OnClickListener() {
+        login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                if (Email.equals(""))
-                {
-                    Email.setError("Enter your Email id");
-                    Email.requestFocus();
-                    return;
-                }
-                if (Pass.equals(""))
-                {
-                    Pass.setError("Enter your Password");
-                    Pass.requestFocus();
-                    return;
+
+                if(Validate()) {
+                    Log.i("Email ID", emailid.getText().toString());
+                    Log.i("Password",password.getText().toString());
+
+                    opendashboard();
                 }
             }
         });
 
-
+        signupp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                opensignup();
+            }
+        });
     }
 
-    public void btn_signupForm(View view) {
-        startActivity(new Intent(getApplicationContext(),Register.class));
+    public void getvalue(){
+        login=(Button)findViewById(R.id.btnsign);
+
+        emailid=(EditText)findViewById(R.id.email);
+        password=(EditText)findViewById(R.id.pass);
+        signupp=(TextView)findViewById(R.id.editsignupp);
+    }
+    public void opendashboard(){
+        Intent intent=new Intent(this,Dash.class);
+        startActivity(intent);
+    }
+    public void opensignup(){
+        Intent intent=new Intent(this,Register.class);
+        startActivity(intent);
+    }
+    private Boolean Validate(){
+        Boolean result =false;
+        String email=emailid.getText().toString();
+        String passwor=password.getText().toString();
+        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+        if(email.isEmpty() || passwor.isEmpty()){
+            Toast.makeText(this,"Please enter valid detail or signup",Toast.LENGTH_SHORT).show();
+        }else if (emailid.getText().toString().trim().matches(emailPattern)) {
+            Toast.makeText(getApplicationContext(),"valid email address",Toast.LENGTH_SHORT).show();
+            result=true;
+        }
+        else {
+            Toast.makeText(getApplicationContext(),"Invalid email address", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return result;
     }
 
-    public void btn_Dash(View view) {
-        startActivity(new Intent(getApplicationContext(),Dash.class));
-    }
 }
+
+
+
+
+
+
+
--- app/src/main/java/com/aditya/upi/spalsh.java
+++ app/src/main/java/com/aditya/upi/spalsh.java
@@ -1,14 +1,23 @@
 package com.aditya.upi;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class spalsh extends AppCompatActivity {
-
+    private static int SPLASH_TIME_OUT=2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spalsh);
+        new Handler().postDelayed(new Runnable()
+        {
+            public void run(){
+                Intent homeIntent = new Intent(spalsh.this,login.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
     }
 }
