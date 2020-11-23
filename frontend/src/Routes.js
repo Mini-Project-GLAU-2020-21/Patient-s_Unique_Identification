e417c9f2c0bf5afe602743d8ba57da1f3c4aefc3
Abhinav Bhardwaj <abhinav.bhardwaj_cs18@gla.ac.in>
2020-11-23 19:09:59 +0530

    after connecting routes with backend

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -3,6 +3,16 @@ import {BrowserRouter, Switch, Route} from "react-router-dom"
 import Home from './core/Home';
 import Signin from './patient/Signin';
 import Signup from './patient/Signup';
+import SignupFailed from './patient/SignupFailed';
+import SignupSuccess from './patient/SignupSuccess';
+import PatientDashboard from './patient/PatientDashboard';
+import PrivateRoute from './auth/helper/PrivateRoutes';
+import PatientProfile from './patient/PatientProfile';
+import UploadDocument  from './patient/UploadDocument';
+import PatientDocuments from './patient/PatientDocuments';
+import PatientSearchByGuest from './core/PatientSearchByGuest';
+import EditProfile from './patient/EditProfile';
+
 
 
 const Routes = () => {
@@ -12,6 +22,15 @@ const Routes = () => {
                 <Route path="/" exact component={Home} />
                 <Route path="/signup" exact component={Signup} />
                 <Route path="/signin" exact component={Signin} />
+                <Route path="/SignupSuccess" exact component={SignupSuccess} />
+                <Route path="/SignupFailed" exact component={SignupFailed} />
+                <Route path="/guest/patient" exact component={PatientSearchByGuest} />
+                <PrivateRoute path="/patient/dashboard" exact component={PatientDashboard} />
+                <PrivateRoute path="/patient/myProfile" exact component={PatientProfile} />
+                <PrivateRoute path="/patient/editProfile" exact component={EditProfile} />
+                <PrivateRoute path="/patient/uploadDocuments" exact component={UploadDocument} />
+                <PrivateRoute path="/patient/myDocuments" exact component={PatientDocuments} />
+ 
             </Switch>
         </BrowserRouter>
     );
