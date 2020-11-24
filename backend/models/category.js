31e8a1325a06ce5041230648e4d95a4cced187ab
Abhinav Bhardwaj <abhinav.bhardwaj_cs18@gla.ac.in>
2020-11-24 16:27:26 +0530

    debugging

--- backend/models/category.js
+++ backend/models/category.js
@@ -9,8 +9,7 @@ const categorySchema = new mongoose.Schema(
             required: true,
             unique: true
         }
-    },
-    {timestamps: true }
+    }
 );
 
 module.exports = mongoose.model("Category", categorySchema);
