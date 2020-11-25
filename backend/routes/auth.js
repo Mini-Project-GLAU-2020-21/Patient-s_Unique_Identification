3ffcae8ecac3650bc9b7ed9617bb4654861f9a73
Abhinav Bhardwaj <abhinav.bhardwaj_cs18@gla.ac.in>
2020-11-25 23:30:13 +0530

    Final file of authentication routes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -9,11 +9,17 @@ const { check, validationResult } = require('express-validator');
 
 router.post(
     "/signup", 
-    [           // if check("name", "name should be atleast of 3 letters.") doesn't works, use   check("name").isLength({ min: 3 }).withMessage('must be at least 3 chars long')
+    [
         check("f_name", "First Name should be at least of 3 char.").isLength({ min: 3 }).custom(value => !/\s/.test(value)),
         check("email", "Valid email is required.").isEmail().custom(value => !/\s/.test(value)),
         check("p_contact_number", "Valid Mobile Number is required.").isLength({ min: 10 }).isMobilePhone('en-IN'),
-        check("password", "password should be at least 5 char.").custom(value => !/\s/.test(value)).isLength({ min: 5 })
+        check("password", "password should be at least 5 char.").custom(value => !/\s/.test(value)).isLength({ min: 5 }),
+        check("dob", "Date of Birth is required.").isLength({ min: 1 }),
+        check("gender", "Gender is required.").isLength({ min: 1 }),
+        check("r_contact_number", "Valid Mobile Number is required.").isLength({ min: 10 }).isMobilePhone('en-IN'),
+        check("r_name", "Name of relative should be at least of 3 char.").isLength({ min: 3 }).custom(value => !/\s/.test(value)),
+        check("address", "Address is required.").isLength({ min: 3 }),
+        check("r_relation", "Relation with relative.").isLength({ min: 3 }).custom(value => !/\s/.test(value))
     ], 
     signup
 );
@@ -21,8 +27,8 @@ router.post(
 router.post(
     "/signin", 
     [           
-        check("email", "email is required.").isEmail(),
-        check("password", "password field is required.").isLength({ min: 1 })
+        check("email", "Email field is required.").isEmail(),
+        check("password", "Password field is required.").isLength({ min: 1 })
     ], 
     signin
 );
