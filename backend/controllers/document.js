4781414bf414bf40ad601d8b2d67bfce26a11b6b
Abhinav Bhardwaj <abhinav.bhardwaj_cs18@gla.ac.in>
2020-11-25 23:22:34 +0530

    Final file of document.js controllers

--- backend/controllers/document.js
+++ backend/controllers/document.js
@@ -2,7 +2,6 @@ const Documents = require("../models/document");
 
 
 exports.getDocumentById = (req, res, next, id) => {
-    
     Documents.findById(id)
     .populate("category")
     .exec((err, document) => {
@@ -18,12 +17,9 @@ exports.getDocumentById = (req, res, next, id) => {
 
 
 
-exports.document_file = (req, res) => {           // method to export photo as it is set as undefined in above method
-    if(req.doc.document_file){                      // checking whether the "product" object has some product or not
-    //console.log(req.doc.document_file.data);
-    //console.log(req.doc.category.name);
+exports.document_file = (req, res) => {           
+    if(req.doc.document_file){
         res.set("Content-Type", req.doc.document_file.contentType)
-        //console.log(req.doc.document_file.data);
         return res.send(req.doc.document_file.data);
     }
 };
